73d4c75aa7232f561acbcb8d2c3d1168c72ba424
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-13 19:48:05 -0300

    feat(session): credential validation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
+import sessionsRouter from './sessions.routes';
 import usersRouter from './users.routes';
 import appointmentsRouter from './appointments.routes';
 
 const router = Router();
 
+router.use('/sessions', sessionsRouter);
 router.use('/users', usersRouter);
 router.use('/appointments', appointmentsRouter);
 
