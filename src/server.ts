c865064bee848e57cd7e4ca8c3363af70563703c
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-13 22:06:35 -0300

    feat(file): route to view files

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 
 import express from 'express';
 import router from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(router);
 
 app.listen(3333, () => {
