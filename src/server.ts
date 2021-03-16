31942d6578dbd5fcc3be343c4ddf437e8f4513ce
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-16 17:37:57 -0300

    build: enabling cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
+
 import router from './routes';
 import uploadConfig from './config/upload';
 
@@ -11,6 +13,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(router);
