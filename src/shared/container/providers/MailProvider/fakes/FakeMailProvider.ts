1d387898ae1d4fe112434ac1ca9a422a73780674
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-24 11:29:52 -0300

    refactor(user): use provider for email templates when sending email forgot password

--- src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
+++ src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
@@ -1,17 +1,10 @@
+import ISendMailDTO from '../dtos/ISendMailDTO';
 import IMailProvider from '../models/IMailProvider';
 
-interface IMassage {
-  to: string;
-  body: string;
-}
-
 export default class FakeMailProvider implements IMailProvider {
-  private messages: IMassage[] = [];
+  private messages: ISendMailDTO[] = [];
 
-  public async sendMail(to: string, body: string): Promise<void> {
-    this.messages.push({
-      to,
-      body,
-    });
+  public async sendMail(message: ISendMailDTO): Promise<void> {
+    this.messages.push(message);
   }
 }
