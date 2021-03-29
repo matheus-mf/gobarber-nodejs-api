350a0952eca73ef1d8ef2a5418defb3ef739c99e
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-29 11:52:25 -0300

    feat(notification): schema and repository creation

---
+++ src/modules/notifications/infra/typeorm/repositories/NotificationRepository.ts
@@ -0,0 +1,29 @@
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import { getMongoRepository, MongoRepository } from 'typeorm';
+
+import Notification from '../schemas/Notification';
+
+class NotificationRepository implements INotificationsRepository {
+  private omrRepository: MongoRepository<Notification>;
+
+  constructor() {
+    this.omrRepository = getMongoRepository(Notification, 'mongo');
+  }
+
+  public async create({
+    content,
+    recipient_id,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = this.omrRepository.create({
+      content,
+      recipient_id,
+    });
+
+    await this.omrRepository.save(notification);
+
+    return notification;
+  }
+}
+
+export default NotificationRepository;
